c738d44fa3f0d0158b6b2ca404cd9761f5a6c3fd
Kirill Donetskov <kirilldonetskov@gmail.com>
2025-04-08 01:52:37 +0600

    Added search and update methods

--- project/dao/user_dao.py
+++ project/dao/user_dao.py
@@ -13,5 +13,32 @@ class UserDAO(Dao):
         query = "INSERT INTO Users (username, email, password, full_name, address, phone_number) VALUES (?, ?, ?, ?, ?, ?)"
         self.execute_query(query, (user.get_username(), user.get_email(), user.get_password(), user.get_name(), user.get_address(), user.get_phone_number()))
 
-        self.__connection.commit()
-        return self.__cursor.lastrowid
+        self._connection.commit()
+        return self._cursor.lastrowid
+    
+    def find_by_username(self, username: str):
+        query = "SELECT * FROM Users WHERE username = ?"
+        row = self.fetch_one(query, (username,))
+        if row:
+            return User(row[4], row[1], row[3], row[5], row[2], row[6])
+        else:
+            return None
+        
+    def find_by_email(self, email: str):
+        query = "SELECT * FROM Users WHERE email = ?"
+        row = self.fetch_one(query, (email,))
+        if row:
+            return User(row[4], row[1], row[3], row[5], row[2], row[6])
+        else:
+            return None
+        
+    def get_all_users(self):
+        query = "SELECT * FROM Users"
+        return self.fetch_all(query)
+    
+    def update_password(self, email: str, new_password: str):
+        query = "UPDATE Users SET password = ? WHERE email = ?"
+        self.execute_query(query, (new_password, email))
+        
+        self._connection.commit()
+        return self._cursor.rowcount
