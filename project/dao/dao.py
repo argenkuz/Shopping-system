8ddf30992c2310e8bae82ccee825c7d818f18cc7
Kirill Donetskov <kirilldonetskov@gmail.com>
2025-04-07 16:28:12 +0600

    Little changes in variable names

--- project/dao/dao.py
+++ project/dao/dao.py
@@ -2,18 +2,18 @@ import sqlite3
 
 class Dao:
     def __init__(self, db_path):
-        self.__connection = sqlite3.connect(db_path)
-        self.__cursor = self.__connection.cursor()
+        self._connection = sqlite3.connect(db_path)
+        self._cursor = self._connection.cursor()
 
     def execute_query(self, query, params=None):
-        self.__cursor.execute(query, params or [])
-        self.__connection.commit()
-        return self.__cursor
+        self._cursor.execute(query, params or [])
+        self._connection.commit()
+        return self._cursor
     
     def fetch_all(self, query, params=None):
-        self.__cursor.execute(query, params or [])
-        return self.__cursor.fetchall()
+        self._cursor.execute(query, params or [])
+        return self._cursor.fetchall()
     
     def fetch_one(self, query, params=None):
-        self.__cursor.execute(query, params or [])
-        return self.__cursor.fetchone()
+        self._cursor.execute(query, params or [])
+        return self._cursor.fetchone()
