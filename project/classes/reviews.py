e0815acf8f399d23ff761bbd6c6e7c4b8ead813f
argenkuz <argenkulzanov@gmail.com>
2025-04-09 15:58:48 +0600

    Added review class

---
+++ project/classes/reviews.py
@@ -0,0 +1,6 @@
+class Reviews:
+    def __init__(self,user_id, product_id, rating, comment):
+        self.__user_id = user_id
+        self.__product_id = product_id
+        self.__rating = rating
+        self.__comment = comment
